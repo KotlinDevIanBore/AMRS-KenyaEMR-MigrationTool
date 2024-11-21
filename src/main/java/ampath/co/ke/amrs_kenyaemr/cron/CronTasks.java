50fdd97bfbd88e314f86d3d46e236faba552eb1a
Rugute <rrugute@gmail.com>
2024-11-21 09:59:34 +0300

    added Triage entity

--- src/main/java/ampath/co/ke/amrs_kenyaemr/cron/CronTasks.java
+++ src/main/java/ampath/co/ke/amrs_kenyaemr/cron/CronTasks.java
@@ -46,26 +46,26 @@ public class CronTasks {
 
     @Autowired
     private AMRSOrderService amrsOrderService;
-    @Scheduled(cron = "0 */1 * ? * *")
+    //@Scheduled(cron = "0 */1 * ? * *")
     public void ProcessUsers() throws JSONException, ParseException, SQLException, IOException {
 
     }
 
-    @Scheduled(cron = "0 0/60 * ? * *")
+    //@Scheduled(cron = "0 */1 * ? * *")
     public void ProcessPatients() throws JSONException, ParseException, SQLException, IOException {
         String locationId="'8cad59c8-7f88-4964-aa9e-908f417f70b2','08feb14c-1352-11df-a1f1-0026b9348838','65bdb112-a254-4cf9-a5a7-29dce997312d','8cad59c8-7f88-4964-aa9e-908f417f70b2'";
         String parentUuid="'8cad59c8-7f88-4964-aa9e-908f417f70b2'";
         MigrateRegistration.patients(server,username,password,locationId,parentUuid,amrsPatientServices,amrsIdentifiersService,OpenMRSURL,auth);
 
     }
-    @Scheduled(cron = "0 0/30 * ? * *")
+    //@Scheduled(cron = "0 */1 * ? * *")
     public void ProcessPrograms() throws JSONException, ParseException, SQLException, IOException {
         String locationId="'8cad59c8-7f88-4964-aa9e-908f417f70b2','08feb14c-1352-11df-a1f1-0026b9348838','65bdb112-a254-4cf9-a5a7-29dce997312d','8cad59c8-7f88-4964-aa9e-908f417f70b2'";
         String parentUuid="'8cad59c8-7f88-4964-aa9e-908f417f70b2'";
         MigrateCareData.programs(server,username,password,locationId,parentUuid,amrsProgramService,amrsPatientServices,OpenMRSURL,auth);
 
     }
-    @Scheduled(cron = "0 0/50 * ? * *")
+    @Scheduled(cron = "0 */1 * ? * *")
     public void ProcessVisits() throws JSONException, ParseException, SQLException, IOException {
         String locationId="'8cad59c8-7f88-4964-aa9e-908f417f70b2','08feb14c-1352-11df-a1f1-0026b9348838','65bdb112-a254-4cf9-a5a7-29dce997312d','8cad59c8-7f88-4964-aa9e-908f417f70b2'";
         String parentUuid="'8cad59c8-7f88-4964-aa9e-908f417f70b2'";
