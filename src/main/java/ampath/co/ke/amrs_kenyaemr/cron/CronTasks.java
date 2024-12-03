39d35d575dab381f1a23e28dbd390e74aa6cb1c7
Geofrey Nyabuto <mwambage@gmail.com>
2024-12-03 17:38:09 +0300

    Updated orders uploading

--- src/main/java/ampath/co/ke/amrs_kenyaemr/cron/CronTasks.java
+++ src/main/java/ampath/co/ke/amrs_kenyaemr/cron/CronTasks.java
@@ -124,15 +124,15 @@ public class CronTasks {
         MigrateCareData.encounterMappings(server,username,password,locationId,parentUuid, amrsEncounterMappingService, amrsPatientServices, amrsConceptMappingService, OpenMRSURL,auth);
 
     }
-   // @Scheduled(cron = "0 */1 * ? * *")
+    @Scheduled(cron = "0 */1 * ? * *")
     public void ProcessOrders() throws JSONException, ParseException, SQLException, IOException {
         String locationId="'8cad59c8-7f88-4964-aa9e-908f417f70b2','08feb14c-1352-11df-a1f1-0026b9348838','65bdb112-a254-4cf9-a5a7-29dce997312d','8cad59c8-7f88-4964-aa9e-908f417f70b2'";
         String parentUuid="'8cad59c8-7f88-4964-aa9e-908f417f70b2'";
-        MigrateCareData.order(server,username,password,locationId,parentUuid, amrsOrderService, amrsPatientServices, amrsEncounterMappingService, amrsConceptMappingService, OpenMRSURL,auth);
+        MigrateCareData.order(server,username,password,locationId,parentUuid, amrsOrderService, amrsPatientServices, amrsEncounterMappingService, amrsConceptMappingService,amrsEncounterService, OpenMRSURL,auth);
 
     }
 
-    @Scheduled(cron = "0 */1 * ? * *")
+//    @Scheduled(cron = "0 */1 * ? * *")
     public void ProcessProgramSwitches() throws JSONException, ParseException, SQLException, IOException {
         String locationId="'8cad59c8-7f88-4964-aa9e-908f417f70b2','08feb14c-1352-11df-a1f1-0026b9348838','65bdb112-a254-4cf9-a5a7-29dce997312d','8cad59c8-7f88-4964-aa9e-908f417f70b2'";
         String parentUuid="'8cad59c8-7f88-4964-aa9e-908f417f70b2'";
