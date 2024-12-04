afeefbf4f13e209dfd0640781b5cdb6cb49bdb1a
Geofrey Nyabuto <mwambage@gmail.com>
2024-12-04 11:22:31 +0300

    Added TCA Processing codes

--- src/main/java/ampath/co/ke/amrs_kenyaemr/cron/CronTasks.java
+++ src/main/java/ampath/co/ke/amrs_kenyaemr/cron/CronTasks.java
@@ -62,6 +62,8 @@ public class CronTasks {
     @Autowired
     private AMRSPatientStatusService amrsPatientStatusService;
 
+    @Autowired
+    private AMRSTCAService amrstcaService;
 
    // @Scheduled(cron = "0 */1 * ? * *")
    //@Scheduled(initialDelay = 0, fixedRate = 30 * 60 * 1000) // Every 30 minutes
@@ -155,12 +157,18 @@ public class CronTasks {
   }
 
 
-    @Scheduled(initialDelay = 0, fixedRate = 30 * 60 * 1000)
+//    @Scheduled(initialDelay = 0, fixedRate = 30 * 60 * 1000)
   public void civilStatus() throws JSONException, ParseException, SQLException, IOException {
         String locationId="'8cad59c8-7f88-4964-aa9e-908f417f70b2','08feb14c-1352-11df-a1f1-0026b9348838','65bdb112-a254-4cf9-a5a7-29dce997312d','8cad59c8-7f88-4964-aa9e-908f417f70b2'";
         String parentUuid="'8cad59c8-7f88-4964-aa9e-908f417f70b2'";
         MigrateCareData.patientStatus(server, username, password, locationId,parentUuid, amrsPatientStatusService, amrsConceptMappingService, amrsPatientServices, OpenMRSURL, auth);
     }
 
+    @Scheduled(initialDelay = 0, fixedRate = 30 * 60 * 1000)
+    public void ProcessTCAs() throws JSONException, ParseException, SQLException, IOException {
+        String locationId="'8cad59c8-7f88-4964-aa9e-908f417f70b2','08feb14c-1352-11df-a1f1-0026b9348838','65bdb112-a254-4cf9-a5a7-29dce997312d','8cad59c8-7f88-4964-aa9e-908f417f70b2'";
+        String parentUuid="'8cad59c8-7f88-4964-aa9e-908f417f70b2'";
+        MigrateCareData.tcas(server,username,password,locationId,parentUuid, amrstcaService, amrsPatientServices, amrsEncounterMappingService, amrsConceptMappingService,amrsEncounterService, OpenMRSURL,auth);
+    }
 
 }
