80d4b8146fc34a55170b767ce2ea33406ed3f83b
Alfred-Mutai <mutaialfr@gmail.com>
2024-12-04 11:13:40 +0300

    Updated enrollments obs concepts

--- src/main/java/ampath/co/ke/amrs_kenyaemr/cron/CronTasks.java
+++ src/main/java/ampath/co/ke/amrs_kenyaemr/cron/CronTasks.java
@@ -146,8 +146,7 @@ public class CronTasks {
         MigrateCareData.programSwitches(server, username, password, locationId,parentUuid, amrsRegimenSwitchService, amrsConceptMappingService, OpenMRSURL, auth);
     }
 
-
-  @Scheduled(cron = "0 */1 * ? * *")
+  //@Scheduled(initialDelay = 0, fixedRate = 30 * 60 * 1000)
   public void ProcessProgramEnrollments() throws JSONException, ParseException, SQLException, IOException {
     String locationId="'8cad59c8-7f88-4964-aa9e-908f417f70b2','08feb14c-1352-11df-a1f1-0026b9348838','65bdb112-a254-4cf9-a5a7-29dce997312d','8cad59c8-7f88-4964-aa9e-908f417f70b2'";
     String parentUuid="'8cad59c8-7f88-4964-aa9e-908f417f70b2'";
